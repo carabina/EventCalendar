eeebd1f526c8d97b0bc3aec0beb30c290424df2a
tommy <tm1291@messiah.edu>
2017-03-01 14:31:58 -0500

    Initial Commit

---
+++ EventCalendar/EventCalendar.h
@@ -0,0 +1,19 @@
+//
+//  EventCalendar.h
+//  EventCalendar
+//
+//  Created by Thomas Martin on 3/1/17.
+//  Copyright © 2017 Thomas Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventCalendar.
+FOUNDATION_EXPORT double EventCalendarVersionNumber;
+
+//! Project version string for EventCalendar.
+FOUNDATION_EXPORT const unsigned char EventCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventCalendar/PublicHeader.h>
+
+
